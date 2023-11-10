5ecf80ee8459e344ec63380b6454c661c2e30b72
girish-devops-project <venkatareddygirish@gmail.com>
2023-11-10 14:40:39 -0500

    Script to prompt the folder path and download the package
    Requirements:
      First, prompt for the folder path. If the folder exists, indicate that the folder exists.
      If the folder doesn't exist, create the folder and download and install a packages in the newly created folder."

---
+++ bash-script/prompt-folder-path-and-download-package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Prompt the user for a folder path
+read -p "Enter the folder path: " folder_path
+
+# Check if the folder already exists
+if [ -d "$folder_path" ]; then
+ echo "Folder already exists."
+else
+ # If the folder doesn't exist, create it
+ mkdir -p "$folder_path"
+
+ # Move into the new folder
+ cd "$folder_path"
+
+ # Download and install a package (e.g., 'example_package' for demonstration)
+ wget https://lnkd.in/gSJmRzFR
+ tar -xzf example_package.tar.gz
+
+ # You can add installation steps here if necessary
+
+ echo "Folder created and package downloaded."
+fi
